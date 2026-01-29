e5c38a57a3281f5c4a3be6407fcf9c888fade81c
desarrollo <mediaob11@gmail.com>
2026-01-29 10:58:10 -0600

    Overwrite nube

---
+++ PdfInspector.Domain/Abstractions/Bitacora/IBitacora.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PdfInspector.Domain.Abstractions.Bitacora
+{
+    /// <summary>
+    /// Bitacora de todos los errores que lanza la aplicación.
+    /// </summary>
+    public interface IBitacora
+    {
+        void LogError(Exception exception);
+    }
+}
